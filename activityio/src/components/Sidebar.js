47cf972b53f57e8a81fad2dff91298f80de256bf
Aryan Chopra <chopraryan@gmail.com>
2021-08-28 23:51:04 +0530

    google fit integration

--- activityio/src/components/Sidebar.js
+++ activityio/src/components/Sidebar.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+import axios from "axios";
 
 const SidebarLink = ({ text, link }) => {
   return (
@@ -10,13 +12,59 @@ const SidebarLink = ({ text, link }) => {
     </Link>
   );
 };
-
 const Sidebar = () => {
+  const [googleUser, setGoogleUser] = useState({
+    loggedin: false,
+    token: null,
+  });
+  useEffect(() => {
+    if (googleUser.loggedin) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${googleUser.token}`,
+          Accept: "application/json",
+        },
+      };
+      const body = {
+        aggregateBy: [
+          {
+            dataTypeName: "com.google.calories.expended",
+          },
+        ],
+        bucketByTime: {
+          durationMillis: 86400000,
+        },
+        endTimeMillis: new Date().getTime(),
+        startTimeMillis: new Date().getTime() - 7 * 86400000,
+      };
+      axios
+        .post(
+          "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
+          body,
+          config
+        )
+        .then((res) => console.log(res));
+    }
+  }, [googleUser]);
+  const responseGoogle = (response) => {
+    if (response.tokenObj) {
+      setGoogleUser({ loggedin: true, token: response.tokenObj.access_token });
+    }
+  };
   return (
     <div className="col-span-2 col-start-1 row-start-1 row-span-2 bg-blue-200 hidden lg:block pt-12">
       <div className="flex flex-col items-center justify-center h-full">
         <SidebarLink text="Add Activity" link="addactivity" />
         <SidebarLink text="Add Project" link="addproject" />
+        {!googleUser.loggedin && (
+          <GoogleLogin
+            clientId=""
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            isSignedIn={true}
+          />
+        )}
       </div>
     </div>
   );
