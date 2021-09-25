4cf13112f0086dff7e52febe38c0636ac5e86b95
Aryan Chopra <chopraryan@gmail.com>
2021-09-26 00:06:30 +0530

    fixed production routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const {
   morganmiddleware,
   errorHandler,
@@ -14,22 +15,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morganmiddleware);
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 app.use("/api/user", userRouter);
 app.use(tokenExtractor);
 app.use("/api/login", loginRouter);
 app.use("/api/project", projectRouter);
 app.use("/api/activity", activityRouter);
 app.use(errorHandler);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", (_, res) => {
-    let url = path.join(__dirname, "client/build", "index.html");
-    // if (!url.startsWith("/app/"))
-    //   // we're on local windows
-    //   url = url.substring(1);
-    res.sendFile(url);
-  });
-}
 
+app.get("*", (_, res) => {
+  let url = path.join(__dirname, "client/build", "index.html");
+  // if (!url.startsWith("/app/"))
+  //   // we're on local windows
+  //   url = url.substring(1);
+  res.sendFile(url);
+});
 module.exports = app;
